cb87911ca536c88d7269adbd9371c7d16722e2f7
Wattelon <Mr.Smayl546@gmail.com>
2022-11-13 22:30:09 +0300

    Мелкие правки инкапсуляции

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    private static int _gold;
+    private static int _revenue;
+    private static int _levelCode;
 
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Menu"))
+        {
+            FindObjectOfType<TextMeshProUGUI>().text = "Золото: " + _gold;
+        }
+        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Map"))
+        {
+
+        }
+        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Game"))
+        {
+
+        }
+    }
 }
